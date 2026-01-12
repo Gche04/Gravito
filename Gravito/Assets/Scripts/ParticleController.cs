35561c0895184de2c6ab42f702fab9e0097697de
Gche04 <95934655+Gche04@users.noreply.github.com>
2026-01-12 22:43:34 +0100

    created Jetpack flame particle

---
+++ Gravito/Assets/Scripts/ParticleController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //jetparticle
+    //get the particle as a child of the player
+    //rotate paricle with motion
+    //toggle on particle with jet on
+}
